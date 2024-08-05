afd683070debe4bdcdd35651ad91acdc69d69596
Joao Senco <joao@senco.dev.br>
2024-08-05 15:27:48 -0300

    ajustes no bg

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footerr from "./components/footer/Footerr";
 
 function App() {
   return (
-      <div className="flex flex-col align-middle h-screen bg-background bg-cover" style={{backgroundImage: `url(https://lh3.googleusercontent.com/pw/AP1GczNpOu1VOaTez2jJbfLzpZUqNUYF8cld0p5mkfPoiX7m3EWt6PS_WdQAZSLV764Q_EhyPLtplXGNCoOFitjWwpQ0SCewzHkAuDDVL6jIcqf2P5ZBKao6Q1LqDJO4jrDICofWNyJIQJ7b68f0nmt-x7ga=w1234-h878-s-no-gm?authuser=0)`}}>
+      <div className="flex flex-col align-middle h-screen bg-background bg-cover" style={{backgroundImage: `url(https://lh3.googleusercontent.com/pw/AP1GczNNX3k5S3Q3yFi9pKbKf8DxmJCO68ZZ-LJwLs42r3xSbFssdFS8LOzM5p7t-eXu4_3eZDTmBDZrO8miDVE5QtxuR6NYfiHjSz5u73V92-LW53bBHsrZwnP3OFq2rPN-gQlY6HGl-kbpc0QfWMdiMugm=w1234-h878-s-no-gm?authuser=0)`}}>
       <Header />
       <Nav/>
       <Github/>
