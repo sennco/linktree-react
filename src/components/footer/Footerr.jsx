418b9c424ff922fc07e3b8ff5cff44fb5bb8b476
Joao Senco <jhsenco@gmail.com>
2024-08-05 13:46:37 -0300

    ajustes no profile

--- src/components/footer/Footerr.jsx
+++ src/components/footer/Footerr.jsx
@@ -2,7 +2,7 @@ const Footerr = () => {
     return (
       <div className="items-center flex flex-col mt-8 text-smoke">
         <h2>
-          Desenvolvido por 
+          Desenvolvido por &nbsp; 
           <a href="https://www.linkedin.com/in/joaohenrique7369151ab/"> 
              João Senco
           </a>
